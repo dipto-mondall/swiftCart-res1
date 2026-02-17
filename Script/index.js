f71f00414379ddd9345917522eeab294d4c0f9af
dipto-mondall <diptomondal12@gmail.com>
2026-02-17 16:21:37 +0600

    added products.html and card container

--- Script/index.js
+++ Script/index.js
@@ -1,1 +1,23 @@
-console.log("connected")
+console.log("connected");
+// show all products
+const allProduct = () =>{
+    const url = "https://fakestoreapi.com/products";
+    fetch(url)
+    .then((res) => res.json())
+    .then((data) => showAllProducts(data))
+}
+
+const showAllProducts =(data)=>{
+  console.log(data)
+  const cardsContainer = document.getElementById("cards-container");
+  console.log(cardsContainer)
+
+  data.forEach(element => {
+    console.log(element)
+    
+  });
+}
+
+allProduct();
+
+
