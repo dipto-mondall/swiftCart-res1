f43e31929ff27ee39d2da17537b7e96fd88d721a
dipto-mondall <diptomondal12@gmail.com>
2026-02-17 22:47:37 +0600

    added all diff. chategorys produts list

--- Script/index.js
+++ Script/index.js
@@ -1,3 +1,33 @@
+// details by modal
+const details = (id) =>{
+  const url = `https://fakestoreapi.com/products/${id}`;
+  fetch(url)
+  .then((res) => res.json())
+  .then((data) => showDetails(data))
+
+};
+
+const showDetails = (d) =>{
+ const modal = document.getElementById("modal-container");
+  modal.innerHTML = "";
+   const div = document.createElement('div');
+   div.innerHTML=`
+   
+            <h3 class="text-lg font-bold text-center">${d.title}</h3>
+        <p class="py-4">${d.description}</p>
+        
+        <div class="flex gap-4  items-center my-4">
+         <p class="my-4">$${d.price}</p>
+         <p class=""><i class="fa-solid fa-star text-orange-500 mr-1"></i>${d.rating.rate}</p>
+        </div>
+         <button class="btn p-5 text-blue-700">Add to Cart</button>
+  
+  `;
+  modal.appendChild(div)
+  my_modal_2.showModal();
+  
+};
+
 // all products
 const allProduct = () => {
   const url = "https://fakestoreapi.com/products";
@@ -6,10 +36,15 @@ const allProduct = () => {
     .then((data) => showAllProducts(data));
 
   document.getElementById("all").classList.add("bg-blue-500", "text-white");
+ 
 };
 
 // show all products
 const showAllProducts = (data) => {
+    document.getElementById("category-2").classList.remove("bg-blue-500", "text-white");
+  document.getElementById("category-3").classList.remove("bg-blue-500", "text-white");
+  document.getElementById("category-4").classList.remove("bg-blue-500", "text-white");
+  document.getElementById("category-5").classList.remove("bg-blue-500", "text-white");
   //console.log(data);
   const cardsContainer = document.getElementById("cards-container");
   cardsContainer.innerHTML = "";
@@ -18,7 +53,7 @@ const showAllProducts = (data) => {
     //console.log(element);
     const div = document.createElement("div");
     div.innerHTML = `
-      <div class="border-1 border-gray-200 p-4 rounded-md ">
+      <div class="border-1 md:h-[690px] border-gray-200 p-4 rounded-md ">
         <!-- img, bg -->
         <div class="bg-gray-400 p-10 flex justify-center items-center">
           <img class="h-[240px] w-full" src="${element.image}" alt="" />
@@ -44,6 +79,7 @@ const showAllProducts = (data) => {
     cardsContainer.appendChild(div);
   });
 };
+
 // load category's
 const category = async () => {
   const url = "https://fakestoreapi.com/products/categories";
@@ -61,7 +97,7 @@ const showCategory = (data) => {
     const li = document.createElement("li");
     li.classList.add("btn", "p-4", "rounded-lg");
     li.id = `category-${i + 2}`;
-    li.setAttribute("onclick", `${data[i]}("${data[i]}")`);
+    li.setAttribute("onclick", `category${i + 2}("${data[i]}")`);
 
     li.innerText = data[i];
 
@@ -69,8 +105,12 @@ const showCategory = (data) => {
   }
 };
 
-const electronics = (c) => {
+// electronics
+const category2 = (c) => {
   document.getElementById("all").classList.remove("bg-blue-500", "text-white");
+  document.getElementById("category-3").classList.remove("bg-blue-500", "text-white");
+  document.getElementById("category-4").classList.remove("bg-blue-500", "text-white");
+  document.getElementById("category-5").classList.remove("bg-blue-500", "text-white");
 
   const cardsContainer = document.getElementById("cards-container");
   cardsContainer.innerHTML = "";
@@ -84,7 +124,7 @@ const electronics = (c) => {
     // console.log(element);
     const div = document.createElement("div");
     div.innerHTML = `
-      <div class="border-1 border-gray-200 p-4 rounded-md ">
+      <div class="border-1 md:h-[690px] border-gray-200 p-4 rounded-md ">
         <!-- img, bg -->
         <div class="bg-gray-400 p-10 flex justify-center items-center">
           <img class="h-[240px] w-full" src="${element.image}" alt="" />
@@ -116,41 +156,155 @@ const electronics = (c) => {
 };
 
 
-// details by modal
-const details = (id) =>{
-  const url = `https://fakestoreapi.com/products/${id}`;
+// jewelery
+const category3 = (c) => {
+   document.getElementById("all").classList.remove("bg-blue-500", "text-white");
+   document.getElementById("category-2").classList.remove("bg-blue-500", "text-white");
+   document.getElementById("category-4").classList.remove("bg-blue-500", "text-white");
+   document.getElementById("category-5").classList.remove("bg-blue-500", "text-white");
+
+  const cardsContainer = document.getElementById("cards-container");
+  cardsContainer.innerHTML = "";
+
+  const url = `https://fakestoreapi.com/products/category/${c}`;
   fetch(url)
-  .then((res) => res.json())
-  .then((data) => showDetails(data))
-
-}
-
-const showDetails = (d) =>{
- const modal = document.getElementById("modal-container");
-  modal.innerHTML = "";
-   const div = document.createElement('div');
-   div.innerHTML=`
-   
-            <h3 class="text-lg font-bold text-center">${d.title}</h3>
-        <p class="py-4">${d.description}</p>
-        
-        <div class="flex gap-4  items-center my-4">
-         <p class="my-4">$${d.price}</p>
-         <p class=""><i class="fa-solid fa-star text-orange-500 mr-1"></i>${d.rating.rate}</p>
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(c);
+       data.forEach((element) => {
+    // console.log(element);
+    const div = document.createElement("div");
+    div.innerHTML = `
+      <div class="border-1 md:h-[690px] border-gray-200 p-4 rounded-md ">
+        <!-- img, bg -->
+        <div class="bg-gray-400 p-10 flex justify-center items-center">
+          <img class="h-[240px] w-full" src="${element.image}" alt="" />
         </div>
-         <button class="btn p-5 text-blue-700">Add to Cart</button>
-  
-  `;
-  modal.appendChild(div)
-  my_modal_2.showModal();
-  
-}
+        <!-- review -->
+        <div class="flex justify-between items-center mt-4">
+          <p class="p-3 bg-gray-100 rounded-2xl">${element.category}</p>
+          <div class="flex items-center gap-3 text-lg">
+            <i class="fa-solid fa-star text-[#FFD700]"></i>
+            <p>${element.rating.rate}  (${element.rating.count}) </p>
+          </div>
+        </div>
+        <!-- price, name -->
+        <h3 class="text-xl font-bold text-gray-600 my-3">${element.title}</h3>
+        <p class="text-black text-xl font-semibold">$ ${element.price}</p>
+        <!-- btn -->
+        <div class="mt-5 flex flex-wrap justify-between items-center text-lg">
+            <button onclick="details(${element.id})" class="btn rounded-xl px-4 text-gray-600 "><i class="fa-regular fa-eye"></i>Details</button>
+            <button onclick="add()" class="btn rounded-xl px-4 text-white bg-violet-600"><i class="fa-solid fa-cart-shopping"></i>Add</button>
+        </div>
+      </div> 
+    `;
+    cardsContainer.appendChild(div);
+  });
 
+    });
 
+    document.getElementById("category-3").classList.add("bg-blue-500", "text-white");
+};
 
+// mens
+const category4 = (c) => {
+   document.getElementById("all").classList.remove("bg-blue-500", "text-white");
+   document.getElementById("category-2").classList.remove("bg-blue-500", "text-white");
+   document.getElementById("category-3").classList.remove("bg-blue-500", "text-white");
+   document.getElementById("category-5").classList.remove("bg-blue-500", "text-white");
 
+  const cardsContainer = document.getElementById("cards-container");
+  cardsContainer.innerHTML = "";
 
+  const url = `https://fakestoreapi.com/products/category/${c}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(c);
+       data.forEach((element) => {
+    // console.log(element);
+    const div = document.createElement("div");
+    div.innerHTML = `
+      <div class="border-1 md:h-[690px] border-gray-200 p-4 rounded-md ">
+        <!-- img, bg -->
+        <div class="bg-gray-400 p-10 flex justify-center items-center">
+          <img class="h-[240px] w-full" src="${element.image}" alt="" />
+        </div>
+        <!-- review -->
+        <div class="flex justify-between items-center mt-4">
+          <p class="p-3 bg-gray-100 rounded-2xl">${element.category}</p>
+          <div class="flex items-center gap-3 text-lg">
+            <i class="fa-solid fa-star text-[#FFD700]"></i>
+            <p>${element.rating.rate}  (${element.rating.count}) </p>
+          </div>
+        </div>
+        <!-- price, name -->
+        <h3 class="text-xl font-bold text-gray-600 my-3">${element.title}</h3>
+        <p class="text-black text-xl font-semibold">$ ${element.price}</p>
+        <!-- btn -->
+        <div class="mt-5 flex flex-wrap justify-between items-center text-lg">
+            <button onclick="details(${element.id})" class="btn rounded-xl px-4 text-gray-600 "><i class="fa-regular fa-eye"></i>Details</button>
+            <button onclick="add()" class="btn rounded-xl px-4 text-white bg-violet-600"><i class="fa-solid fa-cart-shopping"></i>Add</button>
+        </div>
+      </div> 
+    `;
+    cardsContainer.appendChild(div);
+  });
 
+    });
+
+    document.getElementById("category-4").classList.add("bg-blue-500", "text-white");
+};
+// women
+const category5 = (c) => {
+   document.getElementById("all").classList.remove("bg-blue-500", "text-white");
+   document.getElementById("category-2").classList.remove("bg-blue-500", "text-white");
+   document.getElementById("category-3").classList.remove("bg-blue-500", "text-white");
+   document.getElementById("category-4").classList.remove("bg-blue-500", "text-white");
+
+  const cardsContainer = document.getElementById("cards-container");
+  cardsContainer.innerHTML = "";
+
+  const url = `https://fakestoreapi.com/products/category/${c}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(c);
+       data.forEach((element) => {
+    // console.log(element);
+    const div = document.createElement("div");
+    div.innerHTML = `
+      <div class="border-1 md:h-[690px] border-gray-200 p-4 rounded-md ">
+        <!-- img, bg -->
+        <div class="bg-gray-400 p-10 flex justify-center items-center">
+          <img class="h-[240px] w-full" src="${element.image}" alt="" />
+        </div>
+        <!-- review -->
+        <div class="flex justify-between items-center mt-4">
+          <p class="p-3 bg-gray-100 rounded-2xl">${element.category}</p>
+          <div class="flex items-center gap-3 text-lg">
+            <i class="fa-solid fa-star text-[#FFD700]"></i>
+            <p>${element.rating.rate}  (${element.rating.count}) </p>
+          </div>
+        </div>
+        <!-- price, name -->
+        <h3 class="text-xl font-bold text-gray-600 my-3">${element.title}</h3>
+        <p class="text-black text-xl font-semibold">$ ${element.price}</p>
+        <!-- btn -->
+        <div class="mt-5 flex flex-wrap justify-between items-center text-lg">
+            <button onclick="details(${element.id})" class="btn rounded-xl px-4 text-gray-600 "><i class="fa-regular fa-eye"></i>Details</button>
+            <button onclick="add()" class="btn rounded-xl px-4 text-white bg-violet-600"><i class="fa-solid fa-cart-shopping"></i>Add</button>
+        </div>
+      </div> 
+    `;
+    cardsContainer.appendChild(div);
+  });
+
+    });
+
+    document.getElementById("category-5").classList.add("bg-blue-500", "text-white");
+};
 
 allProduct();
+
 category();
