3a85c743ea8d395b4d1b5fe31c633ec5c66fd1ed
dipto-mondall <diptomondal12@gmail.com>
2026-02-17 17:28:24 +0600

    added all products

--- Script/index.js
+++ Script/index.js
@@ -1,23 +1,50 @@
 console.log("connected");
-// show all products
-const allProduct = () =>{
-    const url = "https://fakestoreapi.com/products";
-    fetch(url)
+// all products
+const allProduct = () => {
+  const url = "https://fakestoreapi.com/products";
+  fetch(url)
     .then((res) => res.json())
-    .then((data) => showAllProducts(data))
-}
+    .then((data) => showAllProducts(data));
 
-const showAllProducts =(data)=>{
-  console.log(data)
+    document.getElementById("all").classList.add("bg-blue-500", "text-white");
+
+};
+
+// show all products
+const showAllProducts = (data) => {
+  console.log(data);
   const cardsContainer = document.getElementById("cards-container");
-  console.log(cardsContainer)
+  cardsContainer.innerHTML = "";
 
-  data.forEach(element => {
-    console.log(element)
-    
+  data.forEach((element) => {
+    console.log(element);
+    const div = document.createElement("div");
+    div.innerHTML = `
+      <div class="border-1 border-gray-200 p-4 rounded-md ">
+        <!-- img, bg -->
+        <div class="bg-gray-400 p-10 flex justify-center items-center">
+          <img class="h-[240px] w-full" src="${element.image}" alt="" />
+        </div>
+        <!-- review -->
+        <div class="flex justify-between items-center mt-4">
+          <p class="p-3 bg-gray-100 rounded-2xl">${element.category}</p>
+          <div class="flex items-center gap-3 text-lg">
+            <i class="fa-solid fa-star text-[#FFD700]"></i>
+            <p>${element.rating.rate}  (${element.rating.count}) </p>
+          </div>
+        </div>
+        <!-- price, name -->
+        <h3 class="text-xl font-bold text-gray-600 my-3">${element.title}</h3>
+        <p class="text-black text-xl font-semibold">$ ${element.price}</p>
+        <!-- btn -->
+        <div class="mt-5 flex flex-wrap justify-between items-center text-lg">
+            <button class="btn rounded-xl px-4 text-gray-600 "><i class="fa-regular fa-eye"></i>Details</button>
+            <button class="btn rounded-xl px-4 text-white bg-violet-600"><i class="fa-solid fa-cart-shopping"></i>Add</button>
+        </div>
+      </div> 
+    `;
+    cardsContainer.appendChild(div);
   });
-}
+};
 
 allProduct();
-
-
